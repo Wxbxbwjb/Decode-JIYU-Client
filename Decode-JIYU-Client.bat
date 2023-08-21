28abb579efc25804218c8f7bc3e8dfb776ebdd14
Wxbxbwjb <134978616+Wxbxbwjb@users.noreply.github.com>
2023-08-21 22:45:35 +0800

    Add files via upload

---
+++ Decode-JIYU-Client.bat
@@ -0,0 +1,129 @@
+@echo off
+title FREE OPEN-SOURCE TOOLS By_Wxbxbwjb
+echo FREE OPEN-SOURCE TOOLS By_Wxbxbwjb
+echo.
+taskkill /f /im StudentMain.exe
+echo.
+taskkill /f /im PowerToys.exe
+echo.
+taskkill /f /im ExamClientAgentMK.exe
+echo.
+taskkill /f /im ExamDataClient.exe
+echo.
+taskkill /f /im DesktopHelper.exe
+echo.
+taskkill /f /im DesktopService.exe
+echo.
+taskkill /f /im DesktopClient.exe
+echo.
+sc delete DesktopService
+taskkill /f /im msgc.exe
+echo.
+taskkill /f /im nw.exe
+echo.
+taskkill /f /im Yistart.exe
+echo.
+taskkill /f /im Nmdeputy.exe
+echo.
+del /f /s /q "C:\Program Files (x86)\Company"
+timeout /t 2
+del /f /s /q "C:\Program Files\ksd"
+timeout /t 2
+del /f /s /q "C:\Program Files\iFly Info Tek"
+mshta vbscript:msgbox("已破解！",64+4096,"错误")(window.close)
+timeout /t 3
+taskkill /f /im StudentMain.exe
+echo.
+taskkill /f /im PowerToys.exe
+echo.
+taskkill /f /im ExamClientAgentMK.exe
+echo.
+taskkill /f /im ExamDataClient.exe
+echo.
+taskkill /f /im DesktopHelper.exe
+echo.
+taskkill /f /im DesktopService.exe
+echo.
+taskkill /f /im DesktopClient.exe
+echo.
+sc delete DesktopService
+taskkill /f /im msgc.exe
+echo.
+taskkill /f /im nw.exe
+echo.
+taskkill /f /im Yistart.exe
+echo.
+taskkill /f /im Nmdeputy.exe
+echo.
+del /f /s /q "C:\Program Files (x86)\Company"
+timeout /t 2
+del /f /s /q "C:\Program Files\ksd"
+timeout /t 2
+del /f /s /q "C:\Program Files\iFly Info Tek"
+timeout /t 3
+taskkill /f /im StudentMain.exe
+echo.
+taskkill /f /im PowerToys.exe
+echo.
+taskkill /f /im ExamClientAgentMK.exe
+echo.
+taskkill /f /im ExamDataClient.exe
+echo.
+taskkill /f /im DesktopHelper.exe
+echo.
+taskkill /f /im DesktopService.exe
+echo.
+taskkill /f /im DesktopClient.exe
+echo.
+sc delete DesktopService
+taskkill /f /im msgc.exe
+echo.
+taskkill /f /im nw.exe
+echo.
+taskkill /f /im Yistart.exe
+echo.
+taskkill /f /im Nmdeputy.exe
+echo.
+del /f /s /q "C:\Program Files (x86)\Company"
+timeout /t 2
+del /f /s /q "C:\Program Files\ksd"
+timeout /t 2
+del /f /s /q "C:\艺测考试设置工具"
+timeout /t 2
+del /f /s /q "C:\Program Files\iFly Info Tek"
+timeout /t 3
+taskkill /f /im StudentMain.exe
+echo.
+taskkill /f /im PowerToys.exe
+echo.
+taskkill /f /im ExamClientAgentMK.exe
+echo.
+taskkill /f /im ExamDataClient.exe
+echo.
+taskkill /f /im DesktopHelper.exe
+echo.
+taskkill /f /im DesktopService.exe
+echo.
+taskkill /f /im DesktopClient.exe
+echo.
+sc delete DesktopService
+taskkill /f /im msgc.exe
+echo.
+taskkill /f /im nw.exe
+echo.
+taskkill /f /im Yistart.exe
+echo.
+taskkill /f /im Nmdeputy.exe
+echo.
+del /f /s /q "C:\Program Files (x86)\Company"
+timeout /t 2
+del /f /s /q "C:\Program Files\ksd"
+timeout /t 2
+del /f /s /q "C:\艺测考试设置工具"
+timeout /t 2
+del /f /s /q "C:\Program Files\iFly Info Tek"
+timeout /t 1
+del /f /s /q "C:\艺测考试设置工具"
+timeout /t 1
+del /f /s /q "C:\艺测考试设置工具"
+exit
